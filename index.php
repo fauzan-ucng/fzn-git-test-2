d3bebc6cf5a3b38f4b5dba741a013446b61f67da
Fauzan Al Agung <fauzanalagung@gmail.com>
2022-07-13 12:56:21 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum illum at in explicabo magnam sint nisi harum quam adipisci laboriosam dolorum quibusdam ex amet molestiae, sequi recusandae tenetur voluptatum officiis!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
